8f4324daf7cd68a70ccb4f1f6b7fc4c220904a80
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-01-07 23:11:43 +0530

    made the collecting issues from repos async

--- git.js
+++ git.js
@@ -6,32 +6,53 @@ let browser = new wd.Builder().forBrowser('chrome').build();
 
 
 let finalData = [];
+let totalProjects = 0;
+let projectCovered = 0;
+
 async function getProjectUrls(url, i){
     let browser = new wd.Builder().forBrowser('chrome').build();
     await browser.get(url);
     await browser.wait(wd.until.elementsLocated(wd.By.css(".text-bold.wb-break-word")));
-    let projectBoxes = await browser.findElements(wd.By.css(".text-bold.wb-break-word"));{}
+    let projectBoxes = await browser.findElements(wd.By.css(".text-bold.wb-break-word"));
+    totalProjects += ((projectBoxes.length > 2) ? 2 : projectsBoxes.length)
     finalData[i]["projects"] = [];
     for(let j in projectBoxes){
-        if(j < 2){
-            finalData[i]["projects"].push({projectUrl : (await projectBoxes[j].getAttribute("href"))});
+        if(j == 2){
+            break;
         }
+        finalData[i].projects.push({projectUrl : await projectBoxes[j].getAttribute("href")});
     }
     let projects = finalData[i].projects;
         for(let j in projects){
             getIssues(projects[j].projectUrl, i, j)
         }
+        //check
+        // fs.writeFileSync("finalData.json", JSON.stringify(finalData));
     browser.close();
 }
 
 async function getIssues(url, i, j){
     let browser = new wd.Builder().forBrowser('chrome').build();
-    await browser.get(url);
-    await browser.wait(wd.until.elementsLocated(wd.By.css(".UnderlineNav-item.no-wrap.js-responsive-underlinenav-item.js-selected-navigation-item")));
-    let finalData[i][projects][j]["projectIssues"] = [];
-
-    
-
+    await browser.get(`${url}/issues`);
+    finalData[i].projects[j]["issues"] = [];
+    let issuesBoxes = await browser.findElement(wd.By.css(".Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title"));
+    let currUrl = await browser.getCurrentUrl();
+    if(currUrl == url+"/issues" && issuesBoxes.length != 0){
+        for(let k in issuesBoxes){
+            if(k == 8){
+                break;
+            }
+            let heading = await issuesBoxes[k].getAttribute("innerText");
+            let url = await issuesBoxes[k].getAttribute("href");
+             finalData[i].projects[j].issues.push({"heading" : heading, "url" : url});
+            //finalData[i].projects[j]["issues"].push({"issueHeading" : heading, "url" : url});
+        }
+    }
+    projectCovered +=1;
+    await browser.wait(wd.until.elementsLocated(wd.By.css(".Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title")));
+    if(projectCovered == totalProjects ){
+        fs.writeFileSync("finalData.json", JSON.stringify(finalData));
+    }
     browser.close();
 }
 
@@ -52,7 +73,7 @@ async function main(){
     }
     
     // console.log(finalData);
-    fs.writeFileSync("finalData.json", JSON.stringify(finalData));
+    // fs.writeFileSync("finalData.json", JSON.stringify(finalData));
     browser.close();
 
 }
